e1b8ba56a1d6025c43c282feee93740e9609a949
CharlesP <charles.partous@gmail.com>
2023-02-03 23:38:23 -0500

    layer changing with transform

--- GameJam Powerhouse/Assets/scripts/layerSwitch.cs
+++ GameJam Powerhouse/Assets/scripts/layerSwitch.cs
@@ -6,6 +6,7 @@ public class layerSwitch : MonoBehaviour
 {
     [SerializeField] GameObject ground_layer;
     [SerializeField] GameObject underGround_layer;
+    [SerializeField] GameObject mole;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,17 +18,19 @@ public class layerSwitch : MonoBehaviour
     {
         if (Input.GetKeyDown("space"))
         {
-            if (ground_layer.GetComponent<Renderer>()) //Check if ground or underground is visible
+            if (ground_layer.GetComponent<Renderer>().enabled) //Check if ground or underground is visible
             {
                 ground_layer.GetComponent<Renderer>().enabled = !(ground_layer.GetComponent<Renderer>().enabled); //sets ground layer to visible and then makes it equal to the opposite
                 underGround_layer.GetComponent<Renderer>().enabled = underGround_layer.GetComponent<Renderer>(); //sets underground layer to visible 
-                Debug.Log("switch to underground");
+                mole.transform.position = new Vector3(mole.transform.position.x, mole.transform.position.y, -0.1f);
+                Debug.Log("switch to underground! mole z: " + mole.transform.position);
             }
             else if (underGround_layer.GetComponent<Renderer>().enabled)
             {
                 ground_layer.GetComponent<Renderer>().enabled = ground_layer.GetComponent<Renderer>(); //sets ground layer to visible 
                 underGround_layer.GetComponent<Renderer>().enabled = !(underGround_layer.GetComponent<Renderer>().enabled); //sets underground layer to visible and then makes it equal to the opposite
-                Debug.Log("switch to ground");
+                mole.transform.position = new Vector3(mole.transform.position.x, mole.transform.position.y, -1.1f);
+                Debug.Log("switch to Ground! mole z: " + mole.transform.position);
             }
         }
     }
