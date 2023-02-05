729f039d53ae55ecf659af1d5fd3d7d24988667b
Adamo O <adamo.orsini12@gmail.com>
2023-02-05 17:09:16 -0500

    Fixed main menu

--- GameJam Powerhouse/Assets/scripts/custceneTimer.cs
+++ GameJam Powerhouse/Assets/scripts/custceneTimer.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class custceneTimer : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(waitForEndOfCutscene());
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    IEnumerator waitForEndOfCutscene()
+    {
+        yield return new WaitForSeconds(30);
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
