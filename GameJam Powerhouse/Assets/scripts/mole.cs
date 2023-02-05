47d71cb35acb07e4b8f5d0084c88dda5bd70d278
Adamo O <adamo.orsini12@gmail.com>
2023-02-05 13:58:50 -0500

    Testing fixed update

--- GameJam Powerhouse/Assets/scripts/mole.cs
+++ GameJam Powerhouse/Assets/scripts/mole.cs
@@ -5,13 +5,15 @@ using UnityEngine;
 public class mole : MonoBehaviour
 {
     [SerializeField] Rigidbody2D moleBody;
-    [SerializeField] int movementSpeed;
+    [SerializeField] float movementSpeed;
 
     [SerializeField] AudioClip[] diggingSounds;
     
 
     [SerializeField] float launchSpeed;
     [SerializeField] float maxLaunchSpeed;
+
+    [SerializeField] float launchMultiplier;
 
     [SerializeField] GameObject tailSlingshot;
     [SerializeField] GameObject tail;
@@ -54,8 +56,7 @@ public class mole : MonoBehaviour
         // tailSlingshotRenderer = tailSlingshot.GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         // Reset if walking normally
         if (!isAimingSlingshot)
@@ -95,19 +96,19 @@ public class mole : MonoBehaviour
             {
                 animator.SetBool("front", true);
                 tailAnimator.SetBool("front", true);
-                moleBody.AddForce(new Vector2(0, -movementSpeed), (ForceMode2D)ForceMode.VelocityChange);
+                moleBody.AddForce(new Vector2(0, -movementSpeed) , (ForceMode2D)ForceMode.VelocityChange);
             }
             if (Input.GetKey("a"))
             {
                 animator.SetBool("left", true);
                 tailAnimator.SetBool("left", true);
-                moleBody.AddForce(new Vector2(-movementSpeed, 0), (ForceMode2D)ForceMode.VelocityChange);
+                moleBody.AddForce(new Vector2(-movementSpeed, 0) , (ForceMode2D)ForceMode.VelocityChange);
             }
             if (Input.GetKey("d"))
             {
                 animator.SetBool("right",true);
                 tailAnimator.SetBool("right", true);
-                moleBody.AddForce(new Vector2(movementSpeed, 0), (ForceMode2D)ForceMode.VelocityChange);
+                moleBody.AddForce(new Vector2(movementSpeed, 0) , (ForceMode2D)ForceMode.VelocityChange);
             }
             if (Input.GetKeyUp("w"))
             {
@@ -131,10 +132,90 @@ public class mole : MonoBehaviour
                 }
             }
         }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // // Reset if walking normally
+        // if (!isAimingSlingshot)
+        // {
+        //     animator.SetBool("right", false);
+        //     animator.SetBool("left", false);
+        //     animator.SetBool("front", false);
+        //     animator.SetBool("back", false);
+
+        //     tailAnimator.SetBool("right", false);
+        //     tailAnimator.SetBool("left", false);
+        //     tailAnimator.SetBool("front", false);
+        //     tailAnimator.SetBool("back", false);
+        //     // tail.transform.position = new Vector3(tail.transform.position.x, tail.transform.position.y, 0);
+        //     tail.transform.localPosition = new Vector3(tail.transform.localPosition.x, tail.transform.localPosition.y, 0.015f);
+
+        //     tailSpriteRenderer.enabled = true;
+        // }
+        // // Aiming slingshot, make tail invisible
+        // else
+        // {
+        //     tailSpriteRenderer.enabled = false;
+        // }
+
+        // // Underground, move with wasd
+        // if (transform.position.z == -0.1f) 
+        // { 
+        //     // ** Underground movement **
+        //     if (Input.GetKey("w"))
+        //     {
+        //         animator.SetBool("back", true);
+        //         tailAnimator.SetBool("back", true);
+        //         tail.transform.localPosition = new Vector3(tail.transform.localPosition.x, tail.transform.localPosition.y, -0.01f);
+        //         moleBody.AddForce(new Vector2(0,movementSpeed) * Time.deltaTime, (ForceMode2D)ForceMode.VelocityChange);
+        //     }
+        //     if(Input.GetKey("s"))
+        //     {
+        //         animator.SetBool("front", true);
+        //         tailAnimator.SetBool("front", true);
+        //         moleBody.AddForce(new Vector2(0, -movementSpeed) * Time.deltaTime, (ForceMode2D)ForceMode.VelocityChange);
+        //     }
+        //     if (Input.GetKey("a"))
+        //     {
+        //         animator.SetBool("left", true);
+        //         tailAnimator.SetBool("left", true);
+        //         moleBody.AddForce(new Vector2(-movementSpeed, 0) * Time.deltaTime, (ForceMode2D)ForceMode.VelocityChange);
+        //     }
+        //     if (Input.GetKey("d"))
+        //     {
+        //         animator.SetBool("right",true);
+        //         tailAnimator.SetBool("right", true);
+        //         moleBody.AddForce(new Vector2(movementSpeed, 0) * Time.deltaTime, (ForceMode2D)ForceMode.VelocityChange);
+        //     }
+        //     if (Input.GetKeyUp("w"))
+        //     {
+        //         tail.transform.localPosition = new Vector3(tail.transform.localPosition.x, tail.transform.localPosition.y, 0.015f);
+        //         // tail.transform.position = new Vector3(tail.transform.position.x, tail.transform.position.y, 0);
+        //     }
+
+        //     // ** Digging sound
+
+        //     // Moving, play sound if not playing
+        //     if (moleBody.velocity.magnitude > 0.1)
+        //     {
+        //         if (!digging.isPlaying) {
+        //             int clipIndex = Random.Range(0, 5);
+        //             digging.PlayOneShot(diggingSounds[clipIndex]);  
+        //         }
+        //     } else
+        //     {
+        //         if (digging.isPlaying) {
+        //             digging.Stop();
+        //         }
+        //     }
+        // }
 
         // ** Slingshot **
         // Above ground, can slingshot
-        else if (transform.position.z == -1.1f && canSlingshot)
+        if (transform.position.z == -1.1f && canSlingshot)
+        // else if (transform.position.z == -1.1f && canSlingshot)
         {
             // First press down
             if (Input.GetMouseButtonDown(0)) 
